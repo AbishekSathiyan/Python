8b2055c2286f25b3481cacf2a60e4e8209bf1d6f
AbishekSathiyan <abidinesh2213.gmail.com>
2024-10-16 20:48:30 +0530

    Mark Grade using If-Else

---
+++ If-Else2 Grade.py
@@ -0,0 +1,15 @@
+# 3. **Grading System**
+# **Description:**  
+# Assign a grade based on the marks obtained.
+# **Criteria:**  
+# - 90-100: Grade `A`
+# - 80-89: Grade `B`
+# - 70-79: Grade `C`
+# - 60-69: Grade `D`
+# - Below 60: Grade `F`
+# **Input:**  
+# An integer `marks` (0 ≤ marks ≤ 100).
+# **Output:**  
+# Print the corresponding grade (`A`, `B`, `C`, `D`, or `F`).
+# ---
+
