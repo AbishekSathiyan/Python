28082a9348204bbba531c80ddec7226f10d3be84
AbishekSathiyan <abidinesh2213.gmail.com>
2024-10-16 19:51:49 +0530

    Added If-Else1.py

---
+++ If-Else1.py
@@ -0,0 +1,15 @@
+# Control Structures
+# Conditionals: if, else if, else statements
+
+# # 2. **Leap Year Determination**
+# **Description:**  
+# Determine whether a given year is a leap year.
+# **Rules for Leap Year:**  
+# - A year is a leap year if it is divisible by 4 **and** not divisible by 100, **or** it is divisible by 400.
+# **Input:**  
+# An integer `year`.
+# **Output:**  
+# Print `"Leap Year"` if the year is a leap year, otherwise print `"Not a Leap Year"`.
+# ---
+
+
