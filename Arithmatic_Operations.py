6d3e29735cfbeb520c47576a956a3f1c40972e7c
AbishekSathiyan <abidinesh2213.gmail.com>
2024-10-16 08:24:48 +0530

    Add Arithmatic_Operations.py

---
+++ Arithmatic_Operations.py
@@ -0,0 +1,48 @@
+#Write simple programs to perform arithmetic operations.
+
+print("Addition")
+
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+c= a+b
+
+
+print("Added 2 Values:",c)
+
+
+print("Subtraction")
+
+a=int(input("Enter the First Number: "))
+b=int(input("Enter the Second Number: "))
+c=a-b
+
+print("Subtracted 2 Values:",c)
+
+
+print("Multiplicaton")
+
+
+a=int(input("Enter the First Number: "))
+b=int(input("Enter the Second Number: "))
+c=a*b
+
+print("Multiplied 2 Values:",c)
+
+
+print("Division")
+
+a=int(input("Enter the First Number: "))
+b=int(input("Enter the Second Number: "))
+c=a/b
+
+print("Divided Values:",c)
+
+
+print("Modulo Division")
+
+
+a=int(input("Enter the First Number: "))
+b=int(input("Enter the Second Number: "))
+c=a%b
+
+print("Modulo Division:",c)
