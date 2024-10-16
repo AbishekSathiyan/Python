c046f9014a563d050163dee0e093bfdd8f496b63
AbishekSathiyan <abidinesh2213.gmail.com>
2024-10-16 18:06:49 +0530

    Input Output Function Added

---
+++ Input&Output_Functions.py
@@ -0,0 +1,9 @@
+# Input and Ouput Functions
+# Get Input From the User
+
+groom=input("Groom : Enter Your Name: ")
+bride=input("Bride : Enter Your Name: ")
+
+print("Hi... Welcome to ",groom,bride,"Wedding")
+
+print("Thank You for Come to Our Family Wedding")
