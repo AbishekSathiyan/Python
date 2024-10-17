b4d689ecf56409bb730152fa407b002ba102892d
AbishekSathiyan <abidinesh2213.gmail.com>
2024-10-17 18:55:27 +0530

    If-Else 4 Program Completed

--- Convert Celsius to Fahrenheit.py
+++ Convert Celsius to Fahrenheit.py
@@ -1,3 +1,4 @@
-#Convert temperature from Celsius to Fahrenheit
+#Convert temperature from Celsius to Fahrenheit.
+
 celcius=float(input("Enter Celcius: "))
 print(celcius*1.8+32)
---
+++ If-Else3.py
@@ -0,0 +1,16 @@
+# # 4 **Maximum of Two Numbers**
+# #**Description:**  
+# Find the greater of two numbers.
+# **Input:**  
+# Two integers `a` and `b`.
+# **Output:**  
+# Print the larger number. If both numbers are equal, print `"Equal"`
+
+
+num1=int(input("Enter Number 1:"))
+num2=int(input("Enter Number 2:"))
+
+if num1>num2:
+    print("Number 1",num1,"is Greatest")
+else:
+    print("Number 2",num2,"is Greatest")
